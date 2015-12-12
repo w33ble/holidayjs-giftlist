1384a55b7181daef83e2008e855181eb8af3d359
Troy Mullaney <troy9731@gmail.com>
2015-12-12 16:42:08 -0700

    Finish stylings for homepage

--- assets/js/public/home/HomeCtrl.js
+++ assets/js/public/home/HomeCtrl.js
@@ -1,7 +1,14 @@
 angular.module('giftList').controller('HomeCtrl', HomeCtrl);
+HomeCtrl.$inject = ['HomeModule'];
 
-function HomeCtrl () {
+function HomeCtrl (HomeModule) {
   var vm = this;
 
   vm.panel1Active = true;
+  
+  HomeModule.getUsers().then(function(resp) {
+    vm.users = resp.data;
+  }, function(err) {
+    console.log(err);
+  });
 }
--- assets/js/public/home/HomeMod.js
+++ assets/js/public/home/HomeMod.js
@@ -1,1 +1,16 @@
-angular.module('HomeMod', []);
+angular.module('giftList').factory('HomeModule', HomeModule);
+HomeModule.$inject = ['$http'];
+function HomeModule($http) {
+  var service = {
+    getUsers: getUsers
+  };
+  return service;
+
+  function getUsers() {
+    return $http({
+      method: 'GET',
+      url: '/users'
+    })
+  }
+
+}
