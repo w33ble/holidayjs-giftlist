3ef2000285a793c6052a2850994b93876a32ed73
Vinicio Del Toro <vdeltoropaz@revspringinc.com>
2015-12-12 15:36:19 -0700

    implementing login service

--- assets/js/public/login/LoginCtrl.js
+++ assets/js/public/login/LoginCtrl.js
@@ -1,9 +1,17 @@
 angular.module('giftList').controller('LoginCtrl', LoginCtrl);
+LoginCtrl.$inject = ['loginModule'];
 
-function LoginCtrl () {
+function LoginCtrl (loginModule) {
   var vm = this;
 
   vm.runLogin = function(){
     console.log("login submitted");
+    loginModule.login(vm.email, vm.password)
+    .then(function  (response) {
+      debugger;
+      put(key, value, [options]);
+    }, function (error) {
+      vm.displayError = true;
+    });
   }
 }
