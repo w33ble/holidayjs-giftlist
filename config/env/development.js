3979d83a28af1920171260bfdbab5aa7cbd31396
Joe Fleming <joe.fleming@gmail.com>
2015-12-12 13:14:25 -0700

    configure mongolabs config

--- config/env/development.js
+++ config/env/development.js
@@ -17,8 +17,7 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
-
+  models: {
+    connection: 'devMongodbServer'
+  }
 };
