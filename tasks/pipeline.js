5993340eee27702db80038aa638d59c227cea65f
Frank Barrett <frankebarrett@gmail.com>
2015-12-12 13:41:32 -0700

    Adding lost code

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -40,7 +40,14 @@ var jsFilesToInject = [
 
   'components/angular/angular.min.js',
 
-  'components/foundation/js/foundation.min.js'
+  'components/foundation/js/foundation.min.js',
+
+  'js/private/jamboree/HomeMod.js',
+  'js/private/jamboree/HomeCtrl.js',
+  'js/public/signup/SignupMod.js',
+  'js/public/signup/SignupCtrl.js',
+  'js/public/login/LoginMod.js',
+  'js/public/login/LoginCtrl.js',
 
 
   // Use the "exclude" operator to ignore files
