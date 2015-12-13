f2faa0155aa393735257b9f653965f9dc833bbe9
Troy Mullaney <troy9731@gmail.com>
2015-12-12 17:02:20 -0700

    Add list types for users

--- assets/js/public/home/HomeCtrl.js
+++ assets/js/public/home/HomeCtrl.js
@@ -8,7 +8,19 @@ function HomeCtrl (HomeModule) {
   
   HomeModule.getUsers().then(function(resp) {
     vm.users = resp.data;
+    vm.users.forEach(function(user, i) {
+      HomeModule.getLists(user.id).then(function(resp) {
+        vm.users[i].lists = resp.data;
+        console.log(resp.data);
+      })
+    })
   }, function(err) {
     console.log(err);
+  });
+
+  HomeModule.getList().then(function(resp) {
+    vm.lists = resp.data
+  }, function(err) {
+    console.log(err)
   });
 }
--- assets/js/public/home/HomeMod.js
+++ assets/js/public/home/HomeMod.js
@@ -2,7 +2,9 @@ angular.module('giftList').factory('HomeModule', HomeModule);
 HomeModule.$inject = ['$http'];
 function HomeModule($http) {
   var service = {
-    getUsers: getUsers
+    getUsers: getUsers,
+    getList: getList,
+    getLists: getLists
   };
   return service;
 
@@ -10,6 +12,20 @@ function HomeModule($http) {
     return $http({
       method: 'GET',
       url: '/users'
+    })
+  }
+
+  function getList() {
+    return $http({
+      method: 'GET',
+      url: '/lists'
+    })
+  }
+
+  function getLists(id) {
+    return $http({
+      method: 'GET',
+      url: '/lists/' + id
     })
   }
 
